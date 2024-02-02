c4af6c975d3d5f4b45b8547fcc11019f325b002c
C665967 <corey.kaulenas@gmail.com>
2024-02-01 21:43:00 -0500

    Updated command line arguments with size

--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 
 int main(int argc, char *argv[]) {
     int opt;
-    uint8_t rule = 0;
-    uint32_t iterations = 0;
+    uint8_t rule = 0x00;
+    uint32_t iterations = 10;
+    uint8_t size = 10;
 
-    while ((opt = getopt(argc, argv, "r:i:")) != -1) {
+    while ((opt = getopt(argc, argv, "r:i:s:")) != -1) {
         switch (opt) {
         case 'r':
             rule = (uint8_t)atoi(optarg);
@@ -16,21 +17,25 @@ int main(int argc, char *argv[]) {
         case 'i':
             iterations = (uint32_t)atoi(optarg);
             break;
+        case 's':
+            size = (uint8_t)atoi(optarg);
+            break;
         case '?':
-            if (optopt == 'r' || optopt == 'i') {
+            if (optopt == 'r' || optopt == 'i' || optopt == 's') {
                 fprintf(stderr, "Option -%c requires an argument.\n", optopt);
             } else {
                 fprintf(stderr, "Unknown option: -%c\n", optopt);
             }
             return 1;
         default:
-            fprintf(stderr, "Usage: %s -r <uint8> -i <uint32>\n", argv[0]);
+            fprintf(stderr, "Usage: %s -r <uint8> -i <uint32> -s <uint8>\n", argv[0]);
             return 1;
         }
     }
 
     printf("Value of -r: %u\n", rule);
     printf("Value of -i: %u\n", iterations);
+    printf("Value of -s: %u\n", size);
 
     return 0;
 }
